3d1256c1789638af4832fef38ba38a5316e1f683
beacastroalves <beacastro1008@gmail.com>
2025-11-15 15:29:16 +0000

    criando o array de itens que será mostrado em tela

--- TreinaWebTwitter/src/components/TextInput.jsx
+++ TreinaWebTwitter/src/components/TextInput.jsx
@@ -1,31 +1,39 @@
-import { useRef } from 'react';
+import { useState } from 'react';
 import styles from './TextInput.module.css';
 
 const TextInput = ({placeholder = 'O que está acontecendo?', maxLength = 125, ...props}) => {
 
-  // const [text, setText] = useState('');
+  const [text, setText] = useState('');
+  const [tweetList, setTweetList] = useState([]);
 
-  const inputElement = useRef();
+  const onTextChange = (event) => {
+    const text = event.target.value;
+    if(text.length <= maxLength) {
+      setText(text);
+    }
+  };
 
-  // const onTextChange = (event) => {
-  //   const text = event.target.value;
-  //   if(text.length <= maxLength) {
-  //     setText(text);
-  //   }
-  // }
+  const sendTweet = () => {
+    setTweetList([...tweetList, text]);
+  };
 
   return (
     <div>
       <textarea
-        ref={inputElement}
         className={styles.input}
         placeholder={placeholder}
         maxLength={maxLength}
-        defaultValue={"digite aqui"}
+        value={text}
+        onChange={onTextChange}
         {...props}
       />
-      <p>{10} / {maxLength} </p>
-      <button onClick={() => {console.log(inputElement.current.value)}}>Enviar</button>
+      <p>{text.length} / {maxLength} </p>
+      <button onClick={sendTweet}>Enviar</button>
+      {tweetList.map(tweet => {
+        return (
+          <p>{tweet}</p>
+        )
+      })}
     </div>
   )
 };
