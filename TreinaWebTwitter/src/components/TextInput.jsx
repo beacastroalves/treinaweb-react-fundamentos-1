8e5a349c6bccafa4c332c9e0a7762785f4c158eb
beacastroalves <beacastro1008@gmail.com>
2025-11-15 12:22:13 +0000

    Falando sobre componentes controlado e não controlados

--- TreinaWebTwitter/src/components/TextInput.jsx
+++ TreinaWebTwitter/src/components/TextInput.jsx
@@ -1,28 +1,31 @@
-import { useState } from 'react';
+import { useRef } from 'react';
 import styles from './TextInput.module.css';
 
 const TextInput = ({placeholder = 'O que está acontecendo?', maxLength = 125, ...props}) => {
 
-  const [text, setText] = useState('');
+  // const [text, setText] = useState('');
 
-  const onTextChange = (event) => {
-    const text = event.target.value;
-    if(text.length <= maxLength) {
-      setText(text);
-    }
-  }
+  const inputElement = useRef();
+
+  // const onTextChange = (event) => {
+  //   const text = event.target.value;
+  //   if(text.length <= maxLength) {
+  //     setText(text);
+  //   }
+  // }
 
   return (
     <div>
       <textarea
+        ref={inputElement}
         className={styles.input}
         placeholder={placeholder}
         maxLength={maxLength}
-        value={text}
-        onChange={onTextChange}
+        defaultValue={"digite aqui"}
         {...props}
       />
-      <p>{text.length} / {maxLength} </p>
+      <p>{10} / {maxLength} </p>
+      <button onClick={() => {console.log(inputElement.current.value)}}>Enviar</button>
     </div>
   )
 };
