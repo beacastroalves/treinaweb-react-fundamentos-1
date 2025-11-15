837ec57d085ae092999078439d93b0fc502d8f34
beacastroalves <beacastro1008@gmail.com>
2025-11-15 12:07:35 +0000

    Estados, funções e eventos

--- TreinaWebTwitter/src/components/TextInput.jsx
+++ TreinaWebTwitter/src/components/TextInput.jsx
@@ -1,9 +1,28 @@
+import { useState } from 'react';
 import styles from './TextInput.module.css';
 
 const TextInput = ({placeholder = 'O que está acontecendo?', maxLength = 125, ...props}) => {
+
+  const [text, setText] = useState('');
+
+  const onTextChange = (event) => {
+    const text = event.target.value;
+    if(text.length <= maxLength) {
+      setText(text);
+    }
+  }
+
   return (
     <div>
-      <textarea className={styles.input} placeholder={placeholder} maxLength={maxLength} {...props}/>
+      <textarea
+        className={styles.input}
+        placeholder={placeholder}
+        maxLength={maxLength}
+        value={text}
+        onChange={onTextChange}
+        {...props}
+      />
+      <p>{text.length} / {maxLength} </p>
     </div>
   )
 };
