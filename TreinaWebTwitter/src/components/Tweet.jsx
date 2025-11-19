9ee1e9a7d634ced5a849782c2a3d174bbfbecd56
beacastroalves <beacastro1008@gmail.com>
2025-11-19 10:59:52 +0000

    estilizando o componente tweet

--- TreinaWebTwitter/src/components/Tweet.jsx
+++ TreinaWebTwitter/src/components/Tweet.jsx
@@ -1,13 +1,15 @@
+import styles from './Tweet.module.css';
+
 const Tweet = (props) => {
   return (
-    <div>
-      <img src="https://github.com/beacastroalves.png" />
-      <div>
-        <span>Beatriz Castro</span>
-        <span>@beacastroalves</span>
-        <span>Há 12 horas</span>
+    <div className={styles.tweetContainer}>
+      <img className={styles.avatar} src="https://github.com/beacastroalves.png" />
+      <div className={styles.user}>
+        <span className={styles.userName}>Beatriz Castro</span>
+        <span className={styles.userUserName}>@beacastroalves</span>
+        <span className={styles.date}>Há 12 horas</span>
       </div>
-      <div>
+      <div className={styles.tweetText}>
         {props.children}
       </div>
     </div>
